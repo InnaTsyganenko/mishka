8e402b5c5901032c7535ccc4dd9cbdeed519e005
Nikulichka <poteryaevainna@gmail.com>
2021-01-23 01:55:24 +0300

    Defence, try one

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,25 +1,27 @@
-const productOrderButton = document.querySelector(".promoted-product__button-order");
-const cartButtons = document.querySelectorAll(".catalog__cart-button");
-const modalWindow = document.querySelector(".modal");
-const modalOverlay = document.querySelector(".modal-overlay");
+const productOrderButton = document.querySelector('.promoted-product__button-order');
+const cartButtons = document.querySelectorAll('.catalog__cart-button');
+const modalWindow = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal-overlay');
 
 if (productOrderButton != undefined) {
-  productOrderButton.addEventListener("click", function () {
-  modalWindow.classList.add("modal-active");
-  modalOverlay.classList.add("modal-active");
+  productOrderButton.addEventListener('click', function () {
+  event.preventDefault();
+  modalWindow.classList.add('modal-active');
+  modalOverlay.classList.add('modal-active');
   });
 }
 
 if (cartButtons != undefined) {
   for (i = 0; i < cartButtons.length; i++) {
-    cartButtons[i].addEventListener("click", function () {
-      modalWindow.classList.add("modal-active");
-      modalOverlay.classList.add("modal-active");
+    cartButtons[i].addEventListener('click', function () {
+      event.preventDefault();
+      modalWindow.classList.add('modal-active');
+      modalOverlay.classList.add('modal-active');
     });
   }
 }
 
-modalOverlay.addEventListener("click", function() {
-  modalWindow.classList.remove("modal-active");
-  modalOverlay.classList.remove("modal-active");
+modalOverlay.addEventListener('click', function() {
+  modalWindow.classList.remove('modal-active');
+  modalOverlay.classList.remove('modal-active');
 });
